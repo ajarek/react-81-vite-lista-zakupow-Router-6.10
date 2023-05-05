c25e24d76789b80b22c51c78ea3ea54807bec8c2
Ajarek <ajarek@poczta.onet.pl>
2023-05-05 21:40:05 +0200

    added article

--- src/assets/food.js
+++ src/assets/food.js
@@ -1,112 +1,201 @@
-export const food=[
-    {
-        id:'1',
-        name:'Mięso',
-        image:'img/mieso.png',
-    },
-    {
-        id:'2',
-        name:'Wędlina',
-        image:'img/wedlina.png',
-    },
-    {
-        id:'3',
-        name:'Kiełbasa',
-        image:'img/kiełbasa.png',
-    },
-    {
-        id:'4',
-        name:'Parówki',
-        image:'img/parówki.jpg',
-    },
-    {
-        id:'5',
-        name:'Ser żółty',
-        image:'img/ser.png',
-    },
-    {
-        id:'6',
-        name:'Chleb',
-        image:'img/chleb.png',
-    },
-    {
-        id:'7',
-        name:'Actimel',
-        image:'img/actimel.png',
-    },
-    {
-        id:'8',
-        name:'Mleko',
-        image:'img/mleko.png',
-    },
-    {
-        id:'9',
-        name:'Masło',
-        image:'img/maslo.jpg',
-    },
-    {
-        id:'10',
-        name:'Jaja',
-        image:'img/jaja.png',
-    },
-    {
-        id:'11',
-        name:'Kinder-czekoladki',
-        image:'img/kinderCz.png',
-    },
-    {
-        id:'12',
-        name:'Kinder kanapka',
-        image:'img/kinder-kanapka.jpg',
-    },
-    {
-        id:'13',
-        name:'Pingu',
-        image:'img/pingu.png',
-    },
-    {
-        id:'14',
-        name:'Ciastka',
-        image:'img/ciastka.png',
-    },
-    {
-        id:'15',
-        name:'Batony',
-        image:'img/baton.jpg',
-    },
-    {
-        id:'16',
-        name:'Makaron',
-        image:'img/makaron.jpg',
-    },
-    {
-        id:'17',
-        name:'Koncentrat pomidorowy',
-        image:'img/koncentrat.jpg',
-    },
-    {
-        id:'18',
-        name:'Olej',
-        image:'img/olej.png',
-    },
-    {
-        id:'19',
-        name:'Popcorn',
-        image:'img/popcorn.jpg',
-    },
-    {
-        id:'20',
-        name:'Zupki',
-        image:'img/zupka.png',
-    },
-    {
-        id:'21',
-        name:'Herbata',
-        image:'img/herbata.png',
-    },
-    {
-        id:'22',
-        name:'Przyprawy',
-        image:'img/przyprawy.png',
-    },
+export const food = [
+  {
+    id: '1',
+    name: 'Mięso',
+    image: 'img/mieso.png',
+  },
+  {
+    id: '2',
+    name: 'Wędlina',
+    image: 'img/wedlina.png',
+  },
+  {
+    id: '3',
+    name: 'Kiełbasa',
+    image: 'img/kiełbasa.png',
+  },
+  {
+    id: '4',
+    name: 'Parówki',
+    image: 'img/parówki.jpg',
+  },
+  {
+    id: '5',
+    name: 'Ser żółty',
+    image: 'img/ser.png',
+  },
+  {
+    id: '6',
+    name: 'Chleb',
+    image: 'img/chleb.png',
+  },
+  {
+    id: '7',
+    name: 'Actimel',
+    image: 'img/actimel.png',
+  },
+  {
+    id: '8',
+    name: 'Mleko',
+    image: 'img/mleko.png',
+  },
+  {
+    id: '9',
+    name: 'Masło',
+    image: 'img/maslo.jpg',
+  },
+  {
+    id: '10',
+    name: 'Jaja',
+    image: 'img/jaja.png',
+  },
+  {
+    id: '11',
+    name: 'Kinder-czekoladki',
+    image: 'img/kinderCz.png',
+  },
+  {
+    id: '12',
+    name: 'Kinder kanapka',
+    image: 'img/kinder-kanapka.jpg',
+  },
+  {
+    id: '13',
+    name: 'Pingu',
+    image: 'img/pingu.png',
+  },
+  {
+    id: '14',
+    name: 'Ciastka',
+    image: 'img/ciastka.png',
+  },
+  {
+    id: '15',
+    name: 'Batony',
+    image: 'img/baton.jpg',
+  },
+  {
+    id: '16',
+    name: 'Makaron',
+    image: 'img/makaron.jpg',
+  },
+  {
+    id: '17',
+    name: 'Koncentrat pomidorowy',
+    image: 'img/koncentrat.jpg',
+  },
+  {
+    id: '18',
+    name: 'Olej',
+    image: 'img/olej.png',
+  },
+  {
+    id: '19',
+    name: 'Popcorn',
+    image: 'img/popcorn.jpg',
+  },
+  {
+    id: '20',
+    name: 'Zupki',
+    image: 'img/zupka.png',
+  },
+  {
+    id: '21',
+    name: 'Herbata',
+    image: 'img/herbata.png',
+  },
+  {
+    id: '22',
+    name: 'Przyprawy',
+    image: 'img/przyprawy.png',
+  },
+  {
+    id: '23',
+    name: 'Banany',
+    image: 'img/banany.png',
+  },
+  {
+    id: '24',
+    name: 'Arbuz',
+    image: 'img/arbuz.png',
+  },
+  {
+    id: '25',
+    name: 'Mandarynki',
+    image: 'img/mandarynki.png',
+  },
+  {
+    id: '26',
+    name: 'Truskawki',
+    image: 'img/truskawki.png',
+  },
+  {
+    id: '27',
+    name: 'Marchew',
+    image: 'img/marchew.png',
+  },
+  {
+    id: '28',
+    name: 'Ogórek',
+    image: 'img/ogórek.png',
+  },
+  {
+    id: '29',
+    name: 'Pietruszka',
+    image: 'img/pietruszka.png',
+  },
+  {
+    id: '30',
+    name: 'Pomidory',
+    image: 'img/pomidory.png',
+  },
+  { id: '31',
+   name: 'Ziemniaki',
+    image: 'img/ziemniaki.png',
+  },
+  {
+    id: '32',
+    name: 'Mydło',
+    image: 'img/mydło.png',
+  },
+  {
+    id: '33',
+    name: 'Pasta do zębów',
+    image: 'img/pasta.png',
+  },
+  {
+    id: '34',
+    name: 'Dezodorant',
+    image: 'img/deo.jpg',
+  },
+  {
+    id: '35',
+    name: 'Proszek do prania',
+    image: 'img/proszek.png',
+  },
+  {
+    id: '36',
+    name: 'Ręcznik papierowy',
+    image: 'img/ręcznik.png',
+  },
+  {
+    id: '37',
+    name: 'Papier toaletowy',
+    image: 'img/toaleta.png',
+  },
+  {
+    id: '38',
+    name: 'Tabletki do zmywania',
+    image: 'img/zmywanie.png',
+  },
+  {
+    id: '39',
+    name: 'Żel pod prysznic',
+    image: 'img/żel.png',
+  },
+  {
+    id: '40',
+    name: 'Płyn do naczyń',
+    image: 'img/ludwik.jpg',
+  },
 ]
