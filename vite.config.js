b685929a89d2c4f59615b4f2825fca468b47b560
Ajarek <ajarek@poczta.onet.pl>
2023-05-06 00:25:38 +0200

    correct

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"/react-81-vite-lista-zakupow-Router-6.10/",
+ base:"/react-81-vite-lista-zakupow-Router-6.10/",
   plugins: [react()],
 })
